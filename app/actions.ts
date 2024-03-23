0468f3b2c1cb803985c1e429c1f2ff98bff2062e
me-jameshwartlopez <me@jameshwartlopez.com>
2024-03-23 11:27:19 +0800

    feat: header nav, kinde authentication, shadcn ui application

---
+++ app/actions.ts
@@ -0,0 +1,53 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+import prisma from '@/lib/db';
+import { revalidatePath } from 'next/cache';
+import path from 'path';
+
+export async function createAirbnbHome({ userId }: { userId: string }) {
+  const data = await prisma.home.findFirst({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      createdAT: 'desc',
+    },
+  });
+
+  if (data === null) {
+    const data = await prisma.home.create({
+      data: {
+        userId: userId,
+      },
+    });
+
+    return redirect(`/create/${data.id}/structure`);
+  } else if (
+    !data.addedCategory &&
+    !data.addedDescription &&
+    !data.addedLoaction
+  ) {
+    return redirect(`/create/${data.id}/structure`);
+  } else if (data.addedCategory && !data.addedDescription) {
+    return redirect(`/create/${data.id}/description`);
+  } else if (
+    data.addedCategory &&
+    data.addedDescription &&
+    !data.addedLoaction
+  ) {
+    return redirect(`/create/${data.id}/address`);
+  } else if (
+    data.addedCategory &&
+    data.addedDescription &&
+    data.addedLoaction
+  ) {
+    const data = await prisma.home.create({
+      data: {
+        userId: userId,
+      },
+    });
+
+    return redirect(`/create/${data.id}/structure`);
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { MapFilterItems } from '@/components/MapFilterItems';
+import { Navbar } from '@/components/Navbar';
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
-  return <div className='flex items-center justify-center h-full my-24'>
-    <h1 className='text-center text-4xl md:text-5xl'>Something great is coming to life</h1>
-  </div>;
+  return (
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
+    </div>
+  );
 }
